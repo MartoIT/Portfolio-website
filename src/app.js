dc274a0ee278e3d3745fef19f09a13d17069f86f
Martin <88441320+MartoIT@users.noreply.github.com>
2023-01-05 17:51:28 +0200

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import { render } from '../node_modules/lit-html/lit-html.js';
 import { serviceView } from './views/serviceView.js';
 import { homeView } from './views/homeView.js';
 import { certificateView } from './views/certificatsView.js';
+import { workView } from './views/workView.js';
 const main = document.querySelector('.root')
 page(decoratorContext);
 
 page('/', homeView);
 page('/index.html', homeView);
-page('/Work', () => console.log(`Work`));
+page('/Work', workView);
 page('/Services', serviceView);
 page('/Software', () => console.log(`Software`));
 page('/About me', homeView);
